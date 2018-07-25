b26d97e87621b32fc41313967449e2c0860a6f9d
Kirill.Terentev <Kirill.Terentev@rossko.ru>
2018-07-25 15:22:47 +0300

    Версия без двойных брендов на 1 номер детали

--- frame_partial_reader/frame_partial_reader.py
+++ frame_partial_reader/frame_partial_reader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import warnings
 import pandas as pd
+import json
 
 class Frame_partial_reader():
 
@@ -11,6 +12,57 @@ class Frame_partial_reader():
         self.no_brand_or_group = 0
 
     def read(self,path):#это для фреймов больше 1000
+        fp = open(path, encoding='utf-8')
+        # n = 0
+        #no_brand_or_group = 0
+        #parse_result = {}
+        #dublicates2 = 0
+        #dublicates = 0
+        prev_line = ''
+        for i, line in enumerate(fp):
+
+            if line[:3] == 'NSI':  # если первая позиция в строуке NSI то предыдущая строка закончилась
+
+                splitted = prev_line.split(";")
+                # n += 1
+                oemnumber = splitted[2]
+                brand = splitted[1]
+                group = splitted[5]
+
+                if brand != brand or group != group:  # проверка на Nan
+                    self.no_brand_or_group += 1
+                    continue
+
+                hex = str(hash(brand+group))
+                if oemnumber not in self.parse_result:
+                    # d = dict()
+                    # d['brand'] = brand
+                    # d['group'] =  group
+                    self.parse_result[oemnumber] = {}
+                    self.parse_result[oemnumber][brand] = group.strip()
+                elif oemnumber in self.parse_result:
+
+                    if brand not in self.parse_result[oemnumber] or (brand in self.parse_result[oemnumber] and self.parse_result[oemnumber][brand] != group.strip()):  # такого бренда нет и мы хотим добаватьь еще один или бренд есть но хотим добавить еще одну группу к нему self.parse_result[oemnumber]["2"] = 1
+                        self.dublicates2 += 1  # хотим добавить еще один бренд или под существующим брендом еще одну товарную группу
+                        self.parse_result[oemnumber]["not_one"] = 1
+
+                else:
+                    self.dublicates += 1
+
+                prev_line = line
+
+            else:
+                prev_line = prev_line + line
+
+        fp.close()
+
+
+        #with open('numbers_dictionary.json', 'w') as fp:
+         #   json.dump(self.parse_result, fp)
+
+
+
+    def read2(self,path):#это для фреймов больше 1000
         cols = ['GUID', 'Brand', 'Oemnumber', 'Name', 'MNG_ID', 'MNG_Name', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8',
                 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15',
                 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22',
@@ -27,8 +79,6 @@ class Frame_partial_reader():
 
             goods_classifier = pd.read_csv(path, skiprows = skip, names=cols, nrows = 1000, sep=';',header=None,error_bad_lines=False)
             skip+=1000
-
-
     def parse_part(self,goods_classifier):
         rows = goods_classifier.shape[0]
         for row in range(0,rows,1):
--- searches/explore_goods_classifier.py
+++ searches/explore_goods_classifier.py
@@ -7,14 +7,50 @@ import elastic.elastic_queries_copy
 warnings.filterwarnings('ignore')
 
 
-"""
-fp = open('parts_with_mng.csv')
+
+fp = open('parts_with_mng.csv',encoding='utf-8')
+#n = 0
+no_brand_or_group = 0
+parse_result = {}
+dublicates2 = 0
+dublicates = 0
+prev_line = ''
 for i, line in enumerate(fp):
-    if i == 2279:
-        a = 1
-        break
+
+    if line[:3] =='NSI':#если первая позиция в строуке NSI то предыдущая строка закончилась
+
+        splitted = prev_line.split(";")
+        #n += 1
+        oemnumber = splitted[2]
+        brand = splitted[1]
+        group = splitted[5]
+
+        if brand != brand or group != group:  # проверка на Nan
+            no_brand_or_group += 1
+            continue
+
+        # hex = str(hash(brand+group))
+        if oemnumber not in parse_result:
+            # d = dict()
+            # d['brand'] = brand
+            # d['group'] =  group
+            parse_result[oemnumber] = {}
+            parse_result[oemnumber][brand] = group
+        elif oemnumber in parse_result:
+
+            if brand not in parse_result[oemnumber] or (brand in parse_result[oemnumber] and parse_result[oemnumber][brand] != group):  # такого бренда нет и мы хотим добаватьь еще один или бренд есть но хотим добавить еще одну группу к нему self.parse_result[oemnumber]["2"] = 1
+                dublicates2 += 1  # хотим добавить еще один бренд или под существующим брендом еще одну товарную группу
+
+        else:
+            dublicates += 1
+
+        prev_line = line
+
+    else:
+        prev_line = prev_line+line
+
 fp.close()
-"""
+
 cols = ['GUID', 'Brand', 'Oemnumber', 'Name', 'MNG_ID', 'MNG_Name', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9', 'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', 'Unnamed: 24', 'Unnamed: 25', 'Unnamed: 26', 'Unnamed: 27', 'Unnamed: 28', 'Unnamed: 29', 'Unnamed: 30', 'Unnamed: 31', 'Unnamed: 32', 'Unnamed: 33', 'Unnamed: 34', 'Unnamed: 35', 'Unnamed: 36', 'Unnamed: 37', 'Unnamed: 38', 'Unnamed: 39', 'Unnamed: 40', 'Unnamed: 41']
 
 goods_classifier = pd.read_csv('parts_with_mng.csv', skiprows=1000, names=cols, nrows=3000,sep=';')
